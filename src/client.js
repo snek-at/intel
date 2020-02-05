0db6351656ea2d95eadb2cec46364fabe37f7b34
schettn <nicoschett@icloud.com>
2020-02-05 19:03:20 +0100

    Add interface for github
    It is now possible to send requests to the github graphql API by providing an authorization token, a query and optional variables.

---
+++ src/client.js
@@ -0,0 +1,20 @@
+import { Graphql } from "snek-client";
+
+class Github{
+    constructor(authorization){
+        var url = "https://api.github.com/graphql";
+        let client = new Graphql(url, {
+            authorization
+        })
+        this.get = async (query, variables) => client.send(query, variables);
+    }
+}
+
+export {
+    Github
+}
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
