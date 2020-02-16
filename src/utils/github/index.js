e481d9035b3386c3ffe791705d7ef50e16a94b27
schettn <nicoschett@icloud.com>
2020-02-16 11:10:11 +0100

    Add exports
    Now the converter and the queries are exported.

---
+++ src/utils/github/index.js
@@ -0,0 +1,9 @@
+import * as queries from "./queries";
+import * as converter from "./converter";
+
+export { queries, converter }
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
