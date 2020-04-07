d5fe8b4b0ab57739f143e8facc9f79d996b6da87
schettn <nicoschett@icloud.com>
2020-04-07 20:18:53 +0200

    Add exports
    The needed exports to use the gitlab util are added now.

---
+++ src/utils/gitlab/index.ts
@@ -0,0 +1,9 @@
+import * as paths from './paths';
+import * as converter from './converter';
+
+export { paths, converter };
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
