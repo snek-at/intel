104b001c16d131c1c6087cffa414c5867b81d89e
Pinterid <david.pinterics@gmail.com>
2020-04-17 08:51:33 +0200

    Create GuidGenerator
    The tool for creating random guid string is added.

---
+++ src/toolbox/GuidGenerator.ts
@@ -0,0 +1,27 @@
+//#region > Functions
+/**
+ * @function Generate random guid string.
+ * @returns {string} A guid string.
+ * @description This function generates a random guid string.
+ */
+function GuidGenerator() {
+  let d = new Date().getTime();
+
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    let r = (d + Math.random() * 16) % 16 | 0;
+
+    d = Math.floor(d / 16);
+
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
+//#endregion
+
+//#region > Export
+export default GuidGenerator;
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
