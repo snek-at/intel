952033d61903d99a688c7d4221b55dd0598b1466
sxct <nicoschett@icloud.com>
2020-05-08 03:08:15 +0200

    Add upload util
    Now a upload util is added. 
    Currently only anonfile is supported.
    Currently anonfile is specified for talks.  This must be changed!

---
+++ src/utils/upload/anonfiles.ts
@@ -0,0 +1,87 @@
+//#region > Imports
+//## npm install snek-client
+// Contains a client for web calls
+import { WebClient } from "snek-client";
+//#endregion
+
+//#region > Interfaces
+/** 
+ * @todo Convert this to a generic file interface
+ * @interface Talk defines the structure of a not converted talk object
+ */
+interface ITalk {
+  name: string;
+  displayUrl: string | null;
+  downloadUrl: string | null;
+  html_url: string;
+  path: string;
+  /** @todo Remove repository from the interface */
+  repository: {
+    name: string;
+    full_name: string;
+    html_url: string;
+    owner: {
+      avatar_url: string;
+    };
+  };
+}
+//#endregion
+
+//#region > Functions
+/**
+ * @function
+ * @param {Document} dom A specific download site of anonfile
+ * @returns {string | null} The download URL if available
+ * @description Extracts the download url from a download site
+ */
+function getDownloadUrl(dom: Document): string | null {
+  return dom.getElementsByTagName("a")[1].getAttribute("href");
+}
+
+/**
+ * @function
+ * @param {Blob} file A file to be uploaded
+ * @returns {ITalk} A unconverted talk object
+ * @description Upload a file to anonfile and return 
+ */
+async function uploadFile(file: Blob) {
+  let webClient = new WebClient("https://api.anonfiles.com/");
+  let data = new FormData();
+
+  data.append("file", file);
+
+  const response = await webClient.scraper.post<{
+    data: { file: { metadata: { name: string }; url: { short: string } } };
+  }>("upload", data);
+
+  webClient = new WebClient(response.data.file.url.short);
+  const downloadUrl = getDownloadUrl(await webClient.scraper.getDom(""));
+
+  const talk: ITalk = {
+    name: response.data.file.metadata.name,
+    displayUrl: downloadUrl,
+    downloadUrl: downloadUrl,
+    html_url: response.data.file.url.short,
+    path: "",
+    repository: {
+      name: "",
+      full_name: "",
+      html_url: "",
+      owner: {
+        avatar_url: "",
+      },
+    },
+  };
+
+  return talk;
+}
+//#endregion
+
+//#region > Exports
+export { uploadFile };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019-2020 Simon Prast
+ */
---
+++ src/utils/upload/index.ts
@@ -0,0 +1,14 @@
+//#region > Imports
+//> Anonfiles
+// Contains the upload functionality of anonfiles
+import * as anonfiles from "./anonfiles";
+//#endregion
+
+//#region > Exports
+export { anonfiles };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019-2020 Simon Prast
+ */
