e3d5715998f0860337b97340a3d91d78d70ab13e
Pinterid <david.pinterics@gmail.com>
2020-05-20 14:32:49 +0200

    Update toolbox
    The source code of toolbox was updated to the latest coding style guide.

--- src/toolbox/Delay.ts
+++ src/toolbox/Delay.ts
@@ -1,9 +1,9 @@
 //#region > Functions
 /**
  * @function
- * @param {number} ms A time in milliseconds.
- * @returns {Promise<unknown>} A timeout promise.
- * @description A function to set a timeout.
+ * @param {number} ms A time in milliseconds
+ * @returns {Promise<unknown>} A timeout promise
+ * @description A function to set a timeout
  */
 function Delay(ms: number): Promise<unknown> {
   return new Promise((resolve) => setTimeout(resolve, ms));
