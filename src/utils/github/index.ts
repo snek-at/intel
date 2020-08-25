26f6a6e26b060b7bd46fa66778b8f7864300a676
schettn <nicoschett@icloud.com>
2020-08-25 18:37:58 +0200

    Add provider to github util
    Now the logic which combines the  Github queries
    and converter are now part of the util itself.

--- src/utils/github/index.ts
+++ src/utils/github/index.ts
@@ -1,4 +1,7 @@
 //#region > Imports
+//#PACKAGE "snek-client"
+// Contains the clients for API calls to SNEK, Github and GitLab
+import { GithubClient } from "snek-client";
 //> Queries
 // Serves generated queries
 import * as queries from "./queries";
@@ -7,9 +10,84 @@ import * as queries from "./queries";
 import * as converter from "./converter";
 //#endregion
 
+//#region > Interfaces
+/**
+ *  @interface DataUser defines the structure of the response from
+ *             the github client.
+ */
+interface IDataUser {
+  data: {
+    /**
+     * User: Can contain any information according to a user.
+     *       The content of this object relays on the query with which the
+     *       client gets initialized.
+     */
+    user: object;
+  };
+}
+
+/**
+ * @interface GitHubData defines the structure which is required to run
+ *            the github converter.
+ */
+interface IGitHubData {
+  /**
+   * Profile: A profile object which contains all profile data of a github user.
+   */
+  profile: object;
+  /**
+   * Calendar: A calendar object which contains all calendar data of
+   *           a github user.
+   */
+  calendar: object;
+}
+//#endregion
+
+//#region > Classes
+class Provider {
+  static processSource = async (
+    githubUrl: string,
+    source: {
+      /** User: A username of the provided platform */
+      user: string;
+      /** Authorization: A token for authorizing the client */
+      authorization: string;
+    }
+  ) => {
+    const githubClient = new GithubClient(githubUrl);
+
+    let profileData = (await githubClient.gql.sendQuery(
+      queries.profile(),
+      {
+        username: source.user,
+      },
+      {
+        authorization: source.authorization,
+      }
+    )) as IDataUser;
+
+    let calendarData = (await githubClient.gql.sendQuery(
+      queries.calendar(profileData.data.user as queries.IProfile),
+      {
+        username: source.user,
+      },
+      {
+        authorization: source.authorization,
+      }
+    )) as IDataUser;
+
+    const data: IGitHubData = {
+      profile: profileData.data.user,
+      calendar: calendarData.data.user,
+    };
+
+    await converter.run(data);
+  };
+}
+//#endregion
+
 //#region > Exports
-//> Constant Variables & Functions
-export { queries, converter };
+export default Provider;
 //#endregion
 
 /**
