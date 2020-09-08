ad56d22cc64571520c8d29ceffd2d01389975f94
schettn <nicoschett@icloud.com>
2020-09-08 17:16:02 +0200

    Optimise achievement all
    The error message has been improved, a unused interface
    was removed and the type variable has been removed.

--- src/utils/snek/achievement/index.ts
+++ src/utils/snek/achievement/index.ts
@@ -2,18 +2,14 @@ import Provider from "../index";
 import * as types from "../types";
 import * as queries from "./queries/data";
 
-interface RunnerParameters {}
-
 const all = async () => {
-  const type = "fetch";
-
   try {
     const res = await Provider.client.session.runner<{
       achievements: types.GraphQLAchievement[];
     }>("query", queries.getAchievements, {});
     return res.data ? res.data.achievements : [];
   } catch {
-    throw new Error(`Couldn't successfully ${type} achievements`);
+    throw new Error(`Couldn't successfully fetch achievements`);
   }
 };
 
